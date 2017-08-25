7ce3f826bbec20373080e9b3f6edf0d9bc92b75b
Lubin <1589507430@qq.com>
2017-08-25 09:33:32 +0800

    我

---
+++ GetHotel /GetHotel/InformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.h
+//  GetHotel
+//
+//  Created by admin1 on 2017/8/25.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationViewController : UIViewController
+
+@end
---
+++ GetHotel /GetHotel/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  GetHotel
+//
+//  Created by admin1 on 2017/8/25.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
