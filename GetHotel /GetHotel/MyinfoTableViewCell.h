4d1d7da2beb1572c7a527267ab58395a4beafe96
Lubin <1589507430@qq.com>
2017-08-25 17:00:36 +0800

    导航栏

---
+++ GetHotel /GetHotel/MyinfoTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MyinfoTableViewCell.h
+//  GetHotel
+//
+//  Created by admin1 on 2017/8/25.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyinfoTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *leftIcon;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
