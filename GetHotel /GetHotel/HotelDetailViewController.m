df47a4dd4ab97f3ef5fef6afcf0cf1f85b2c4bac
Chensiyu <2737581039@qq.com>
2017-08-27 14:43:57 +0800

    detail部分功能

--- GetHotel /GetHotel/HotelDetailViewController.m
+++ GetHotel /GetHotel/HotelDetailViewController.m
@@ -18,6 +18,17 @@
 @property (weak, nonatomic) IBOutlet UIButton *chooseHotelBtn;
 @property (weak, nonatomic) IBOutlet UILabel *inTimeLbl;
 @property (weak, nonatomic) IBOutlet UILabel *outTimeLbl;
+@property (weak, nonatomic) IBOutlet UIButton *payBtn;
+- (IBAction)payAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIButton *chatingBtn;
+- (IBAction)chatingAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)mapAction:(UIButton *)sender forEvent:(UIEvent *)event;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *confirmBtn;
+- (IBAction)confirmAction:(UIBarButtonItem *)sender;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelBtn;
+- (IBAction)cancelAction:(UIBarButtonItem *)sender;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
 @end
 
@@ -31,6 +42,22 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(void)setDefaultDateForButton{
+    //初始化日期格式器
+    NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
+    //定义日期格式
+    formatter.dateFormat=@"yyyy-MM-dd";
+    //当前时间
+    NSDate *date=[NSDate date];
+    //明天的日期
+    NSDate *dateTom=[NSDate dateTomorrow];
+    //将时间转换为字符串
+    NSString *dateStr=[formatter stringFromDate:date];
+    NSString *dateTomStr=[formatter stringFromDate:dateTom];
+    //将处理好的时间字符串设置给两个button
+   // [_startDateBrn setTitle:dateStr forState:UIControlStateNormal];
+   // [_endDateBtn setTitle:dateTomStr forState:UIControlStateNormal];
 }
 - (void)naviConfig {
     //设置导航条标题文字
@@ -56,4 +83,55 @@
 }
 */
 
+- (IBAction)payAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    
+    //获取要跳转的页面实例
+    UINavigationController *detailToPay=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToPay"];
+    //执行跳转
+    [self presentViewController:detailToPay animated:YES completion:nil];
+//    if ([Utilities loginCheck]) {
+    //        获取要跳转的页面实例
+//    UINavigationController *detailToUserAddress=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToUserAddress"];
+//    执行跳转
+ //   [self presentViewController:detailToUserAddress animated:YES completion:nil];
+//    }else{
+//      [Utilities popUpAlertViewWithMsg:@"该功能需要登录才会开放，请您登录" andTitle:@"提示" onView:self];
+//    }
+}
+- (IBAction)chatingAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    //获取要跳转的页面实例
+    UINavigationController *detailToTalk=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToTalk"];
+    //执行跳转
+    [self presentViewController:detailToTalk animated:YES completion:nil];
+    //    if ([Utilities loginCheck]) {
+    //        获取要跳转的页面实例
+    //    UINavigationController *detailToUserAddress=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToUserAddress"];
+    //    执行跳转
+    //   [self presentViewController:detailToUserAddress animated:YES completion:nil];
+    //    }else{
+    //      [Utilities popUpAlertViewWithMsg:@"该功能需要登录才会开放，请您登录" andTitle:@"提示" onView:self];
+    //    }
+}
+
+- (IBAction)mapAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    //获取要跳转的页面实例
+    UINavigationController *detailToUserAddress=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToUserAddress"];
+    //执行跳转
+    [self presentViewController:detailToUserAddress animated:YES completion:nil];
+    //    if ([Utilities loginCheck]) {
+    //        获取要跳转的页面实例
+    //    UINavigationController *detailToUserAddress=[Utilities getStoryboardInstance:@"HotelDetail" byIdentity:@"DetailToUserAddress"];
+    //    执行跳转
+    //   [self presentViewController:detailToUserAddress animated:YES completion:nil];
+    //    }else{
+    //      [Utilities popUpAlertViewWithMsg:@"该功能需要登录才会开放，请您登录" andTitle:@"提示" onView:self];
+    //    }
+}
+- (IBAction)confirmAction:(UIBarButtonItem *)sender {
+    
+}
+- (IBAction)cancelAction:(UIBarButtonItem *)sender {
+    _toolBar.hidden=YES;
+    _datePicker.hidden=YES;
+}
 @end
